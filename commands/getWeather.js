57716a2d350f1e0a6d58d1c356e8a2bf246fdff1
Katia <katia.zakharina@gmail.com>
2022-06-27 00:54:48 +0300

    feat: add api error handling

--- commands/getWeather.js
+++ commands/getWeather.js
@@ -1,22 +1,29 @@
 import apiService from '../services/api.service.js';
 import storageService from '../services/storage.service.js';
+import logService from '../services/log.service.js';
 
 export const getWeather = async () => {
   const token = await storageService.getKeyValue('token');
   const city = await storageService.getKeyValue('city');
 
   if (!token) {
-    throw new Error(
-      'Token not specified, to set it use command -t [API_KEY]'
-    );
+    throw new Error('Token not specified, to set it use command -t [API_KEY]');
   }
 
   if (!city) {
-    throw new Error(
-      'City not specified, to set it use command -c [CiTY]'
-    );
+    throw new Error('City not specified, to set it use command -c [CiTY]');
   }
 
-  const data = await apiService.getWeather(token, city);
-  console.log(data);
+  try {
+    const data = await apiService.getWeather(token, city);
+    console.log(data);
+  } catch (error) {
+    if (error?.response?.status == 404) {
+      logService.printError('City is set incorrectly');
+    } else if (error?.response?.status == 401) {
+      logService.printError('Token is set incorrectly');
+    } else {
+      logService.printError(error.message);
+    }
+  }
 };
